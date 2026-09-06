3d1b601c94099fece343f8066744ca8564be2e18
Pranay <pranayvjadhao197@gmail.com>
2026-09-06 22:33:15 +0530

    Initial commit

---
+++ pattern.py
@@ -0,0 +1,94 @@
+'''
+# Even Numbers
+num = 2
+for i in range(1,6,1):
+    for j in range(1,i+1):
+        print(num, end = " ")
+        num = num + 2
+    print()
+
+num = 1
+for i in range(1,6,2):
+    for j in range(1,i+1):
+        print(num, end =" ")
+        num = num + 1
+    print()"""
+
+"""for i in range (1,6,1):
+    for j in range(i):
+        print(chr(65+j), end=" ")
+    print()"""
+
+"""
+for i in range (1,6,1):
+    for j in range(i):
+        print(chr(64+i), end=" ")
+    print()"""
+
+
+"""
+Binary Number
+for i in range (1,6,1):
+    for j in range (1,i+1):
+        if (i+j)%2==0:
+            print(1,end=" ")
+        else:
+            print(0, end=" ")
+    print()"""
+
+"""for i in range (1,6,1):
+    for j in range(1, i+1):
+        if (i+j)%2==0:
+            print(1,end=" ")
+        else:
+            print(0,end=" ")
+    print()"""
+
+"""for i in range(1,5,1):
+    for j in range(i):
+        print("*",end=" ")
+    print()
+
+print()"""
+'''
+'''
+for i in range (1,6,1):
+    for j in range (i):
+        print("*", end=" ")
+print()
+'''
+
+'''for i in range (5,0,-1):
+    for j in range(i):
+        print("*", end= ' ')
+    print()'''
+
+'''for i in range(1,6,1):
+    for j in range (5-i):
+        print(" ", end=' ')
+    for j in range (i):
+        print('*', end=' ') 
+    print()'''
+'''
+for i in range (5, 0, -1):
+    for j in range(5-i):
+        print(" ", end=' ')
+    for j in range (i):
+        print("*", end=' ')
+    print()'''
+
+"""for i in range (1,6,1):
+    for j in range (5-i):
+        print(" ", end= " ")
+    for j in range (2*i-1):
+        print('*', end=' ')
+    print()
+for i in range(4,0,-1):
+    for j in range(5-i):
+        print(' ', end=" ")
+    for j in range (2*i-1):
+        print("*", end=" ")
+    print()
+"""
+'''
+'''
